6bef31d060b2e18531d470f6e604e3579bc96e0c
MD941 <dinomirt1@gmail.com>
2024-10-10 20:23:44 +0200

    Do while petlja - traženje sretnog broja

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("commit");
+
+        int tajniBroj = 5;
+        Scanner scanner = new Scanner(System.in);
+        int korisnikovInput;
+        do {
+            System.out.println("Pogodite tajni broj između 1 do 10");
+            korisnikovInput = scanner.nextInt();
+
+            if (tajniBroj == korisnikovInput) {
+                System.out.println("Pogodili ste tajni broj!");
+            } else {
+                System.out.println("Nažalost nije točno, pokušajte ponovo!");
+            }
+        } while (tajniBroj != korisnikovInput);
     }
 }
