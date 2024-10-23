72b3ed85e760856eb9164980f1dfbf9077d520be
MD941 <dinomirt1@gmail.com>
2024-10-23 20:57:41 +0200

    Trazenje duplikata

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,40 @@
 package org.example;
 
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Unesite broj: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Unesite željenu veličinu polja: ");
-        int input = scanner.nextInt();
-        System.out.println("Unesite sljedeći broj znakova: " + input);
-        int[] arr = new int[input];
-        for (int i = 0; i < input; i++) {
-            arr[i] = scanner.nextInt();
+        int trazeniBroj = scanner.nextInt();
+
+        List<Integer> listaBrojeva = new ArrayList<>(trazeniBroj);
+//        2 4 5 6 5
+        for (int i = 0; i < trazeniBroj; i++) {
+            listaBrojeva.add(scanner.nextInt());
         }
-        System.out.println("Maksimalna vrijednost u polju je: " + Arrays.stream(arr).max().getAsInt());
+//2-4       5-6
+//          5-5      6-5
+//
+//2-5
+//2-6
+//2-5
+//
+//4-5
+//4-6
+//4-6
+        for (int j = 0; j < listaBrojeva.size(); j++) {
+            int brojKojiProvjeravamo = listaBrojeva.get(j);
+
+            for (int z = j + 1; z < listaBrojeva.size() - 1; z++) {
+                int broj = listaBrojeva.get(z);
+                boolean isIt = brojKojiProvjeravamo ==  broj;
+            }
+
+
+        }
     }
 }
