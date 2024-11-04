b9d5ec3389b86ab8abf9e1e293f3b249a092c560
MD941 <dinomirt1@gmail.com>
2024-11-04 17:34:17 +0100

    Zadaca OOP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,18 @@
 package org.example;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Unesite zeljenu velicinu liste: ");
-        Scanner scanner = new Scanner(System.in);
-        int velicinaListe = scanner.nextInt();
-        List<Integer> listaBrojeva = new ArrayList<>(velicinaListe);
-        System.out.println("Unesite listu sljedece velicine: " + velicinaListe);
-        for (int i = 0; i < velicinaListe; i++) {
-            listaBrojeva.add(scanner.nextInt());
-        }
-        izracunaj(listaBrojeva);
-        System.out.println(izracunaj(listaBrojeva) ? "Lista je uzlazno sortirana" : "Lista nije uzlazno sortirana");
-    }
 
-    public static Boolean izracunaj(List<Integer> lista) {
-        for (int i = 0; i < lista.size() - 1; i++) {
-            if (lista.get(i) > lista.get(i + 1)) {
-                return false;
-            }
+            Polaznik polaznik1 = new Polaznik("Ivo", "Ivić");
+            System.out.println(polaznik1.getIme() + " " + polaznik1.getPrezime());
+
+            Polaznik polaznik2 = new Polaznik("Petar", "Perić", 25, "M");
+            polaznik2.setDob(30);
+
+            Polaznik polaznik3 = new Polaznik();
+            polaznik3.setIme("Marko");
+            polaznik3.setPrezime("Markić");
         }
-        return true;
     }
-}
