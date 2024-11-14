ae829c5b5f0d5adaeb94bee764f4e6196d9eb1fc
MD941 <dinomirt1@gmail.com>
2024-11-14 20:41:57 +0100

    Predavanje 14.11. - brojanja ASCII znakova

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,37 @@
 package org.example;
 
 
-import java.math.BigDecimal;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        TekuciRacun tekuciRacun1 = new TekuciRacun("48846846468", BigDecimal.valueOf(186.3), "Ivan");
-        tekuciRacun1.isplata(BigDecimal.valueOf(100));
-        tekuciRacun1.getStanje();
-    }
+        Student student1 = new Student("Ivan", "Horvat", "23232133");
+        Student student2 = new Student("Matija", "Lukec", "23232132");
+        Student student3 = new Student("Ivan", "Kukec", "232321311");
 
-    }
 
+        try (PrintWriter out = new PrintWriter("studenti.txt")) {
+            out.println(student1.getIme() + " " + student1.getPrezime() + " " + student1.getBrIndexa());
+            out.println(student2.getIme() + " " + student2.getPrezime() + " " + student2.getBrIndexa());
+            out.println(student3.getIme() + " " + student3.getPrezime() + " " + student3.getBrIndexa());
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        int c = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("studenti.txt"))) {
+            while (bufferedReader.read() != -1) {
+                c++;
+            };
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        ;
+        System.out.println(c);
+    }
+}
 
 
 
